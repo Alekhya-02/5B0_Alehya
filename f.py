6ec3dcdefbb62ed55796812877b9177fb5288858
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-20 15:30:58 +0530

    Add files via upload

---
+++ f.py
@@ -0,0 +1,16 @@
+#unexpected indent
+a=10
+b=20
+print(a+b)
+if a<b:
+    print("a is samll")
+else:
+    print("a is big")
+
+#Expected Output
+    #30
+    #a is small
+
+#Actual Output
+    #30
+    #a is small
