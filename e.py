384519771192fd482fe16dbb8e219d078af6a38e
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-20 12:31:12 +0530

    Add files via upload

---
+++ e.py
@@ -0,0 +1,14 @@
+a=int(input())#10
+b=int(input())#20
+c=int(input())#30
+sum=(a+b+c)
+average=(sum/3)
+print('sum=',sum)
+print('average=',average)
+#expected output
+#a+b+c=60
+#(a+b+c)/3)=20.0
+
+#output
+#a+b+c=60
+#(a+b+c)/3=20.0
