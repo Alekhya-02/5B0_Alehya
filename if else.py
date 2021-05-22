94d3c98636755075d6db7f1ad64a9b0e7b924653
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-22 12:41:41 +0530

    Update if else.py

--- if else.py
+++ if else.py
@@ -7,4 +7,14 @@ else:
       print ("HELLO",name)
       print ("you are ELIGIBLE for vote due to")
 
-      
+#expected output
+#Enter input Harry
+#Enter age 18
+#HELLO Harry
+#you are ELIGIBLE for vote due to 18
+
+#Enter input Mr.potter
+#Enter age 8
+#SORRY! Mr.potter
+#You are NOT ELIGIBLE for Vote
+     
