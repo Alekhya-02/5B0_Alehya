f9a510b1b876350d2582baf356a5f704226f6833
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-22 14:33:45 +0530

    Add files via upload

---
+++ if else if.py
@@ -0,0 +1,27 @@
+a=int(input('enter nuber'))
+if(a%2==0) :
+      print('divisible by 2')
+else:
+    if(a%3==0):
+        print('divisible by 3')
+    elif(a%4==0):
+        print('divisible by 4')
+    elif (a%5==0):
+        print('divisible by 5')
+        
+        
+    
+      
+    
+        
+    
+
+      
+      
+      
+
+   
+    
+
+
+
