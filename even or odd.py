cc76474e41b472b68aaf3f93b2e72fddbc3f901a
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-22 13:50:02 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,11 @@
+a=int(input('enter number '))
+if a%2==0 :
+    print('a is an even number')
+else :
+    print('a is an odd number')
+
+#expected output
+#enter number 622
+#a is an even number
+#enter number 3147
+#a is an odd number   
