0a4dcb0e01b8e77541371296423715ed615c64db
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-22 12:27:53 +0530

    Add files via upload

---
+++ if else.py
@@ -0,0 +1,10 @@
+name=input("Enter input")#string value
+age=int(input("Enter age"))#integer value
+if age<=18:
+      print("SORRY!",name)
+      print("You are NOT ELIGIBLE for Vote")
+else:
+      print ("HELLO",name)
+      print ("you are ELIGIBLE for vote due to")
+
+      
