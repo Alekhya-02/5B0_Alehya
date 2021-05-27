e72385dadd37949db72a8cda8de9ff9e39123c2a
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-27 12:29:22 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+start=int(input('enter starting value'))
+end=int(input('enter ending value'))
+step_count=int(input('enter step count'))
+while start<=end:
+    print(start)
+    start=start+step_count
+    
+    
