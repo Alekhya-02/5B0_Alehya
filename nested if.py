7b22b1425da8239a1960479cb1aa56df28da09bf
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-27 11:49:10 +0530

    Add files via upload

---
+++ nested if.py
@@ -0,0 +1,24 @@
+username=input('Enter usernsme')
+password=input('Enter Password')
+if username=="CSEB":
+    if password=="AEC":
+        print ("VALID STUDENT")
+    else :
+        print ("INVALID PASSWORD")
+else:
+     print("INVALID USERNAME")
+
+#expected output
+#Enter usernsmeCSEB
+#Enter PasswordAEC
+#VALID STUDENT
+
+#Enter usernsmeALEKHYA
+#Enter PasswordAEC
+#INVALID USERNAME
+
+#Enter usernsmeCSEB
+#Enter Password123
+#INVALID PASSWORD
+        
+    
