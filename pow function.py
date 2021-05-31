e65fc233c6e8a627a5571e17ac49b06e985ef176
Alekhya-02 <84336949+Alekhya-02@users.noreply.github.com>
2021-05-31 09:24:39 +0530

    Add files via upload

---
+++ pow function.py
@@ -0,0 +1,18 @@
+base=int(input('enter num1'))
+exponent=int(input('enter num2'))
+result=pow(base,exponent)
+print(result)
+remainder=result%10
+print("The last digit of the number is :{}".format(remainder))
+
+
+    
+#enter num16
+#enter num22
+#36
+#The last digit of the number is :6
+
+#enter num17
+#enter num24
+#2401
+#The last digit of the number is :1
